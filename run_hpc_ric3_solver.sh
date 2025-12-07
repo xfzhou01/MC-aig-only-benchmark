9ae2124692a02709b959e08f9bba24a0d109abbb
xfzhou01 <xzhoubu@connect.ust.hk>
2025-12-07 20:03:55 +0800

    20251207 tmp

--- run_hpc_ric3_solver.sh
+++ run_hpc_ric3_solver.sh
@@ -43,12 +43,12 @@ done
 echo "Running with $PARALLEL_JOBS parallel jobs"
 
 # Create a directory to store logs
-LOG_DIR="hpc_ric3_sl_mab_6_add_context_and_reward_decay070"
+LOG_DIR="hpc_ric3_mab_2025"
 mkdir -p "$LOG_DIR"
 
 # Find all .aig and .aag files recursively
 echo "Finding all AIGER files (.aig and .aag)..."
-AIGER_FILES=$(find /hpc/home/cwb.xzhoubu/MC_HWMCC_benchmark_simple -name "*.aig" -o -name "*.aag")
+AIGER_FILES=$(find /hpc/home/cwb.xzhoubu/hwmcc2025 -name "*.aig" -o -name "*.aag")
 TOTAL_FILES=$(echo "$AIGER_FILES" | wc -l)
 echo "Found $TOTAL_FILES AIGER files."
 
@@ -81,9 +81,9 @@ process_file() {
         ABSOLUTE_FILE=$(realpath "$FILE")
         #COMMAND="bsub -Ip -n 1 -m "$CPU_HOSTS" docker run --rm  -v "$ABSOLUTE_FILE":/root/model.aig 10.120.24.15:5000/jhinno/ric3:latest -e ic3 --ic3-dynamic /root/model.aig 2>&1"
         #bsub -Ip -n 1 -m "$CPU_HOSTS" docker run --rm  -v "$ABSOLUTE_FILE":/root/model.aig 10.120.24.15:5000/jhinno/ric3:latest -e ic3 --ic3-dynamic /root/model.aig 2>&1
-	COMMAND="bsub -Ip -n 1 /hpc/home/cwb.xzhoubu/rIC3/rIC3  -e ic3 --ic3-enable-ctx-mab --ic3-no-pred-prop "$ABSOLUTE_FILE" 2>&1"
+	COMMAND="bsub -Ip -n 1 /hpc/home/cwb.xzhoubu/rIC3/rIC3  -e ic3 --ic3-enable-ctx-mab  --ic3-no-pred-prop "$ABSOLUTE_FILE" 2>&1"
         # actually, IC3REF can be executed on any CPU HOSTS can does not require any docker to be pulled there
-        bsub -Ip -n 1 "timeout 3600 /hpc/home/cwb.xzhoubu/rIC3/rIC3  -e ic3 --ic3-enable-ctx-mab --ic3-no-pred-prop "$ABSOLUTE_FILE"" 2>&1
+        bsub -Ip -n 1 "timeout 3600 /hpc/home/cwb.xzhoubu/rIC3/rIC3  -e ic3 --ic3-enable-ctx-mab   --ic3-no-pred-prop "$ABSOLUTE_FILE"" 2>&1
         echo "File: $FILE"
         echo "$COMMAND"
         
